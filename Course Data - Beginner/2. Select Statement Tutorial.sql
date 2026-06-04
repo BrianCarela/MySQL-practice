f18913f3270f4aa57fbd4a27aa1a703298538edc
Brian Carela <BrianCarela@gmail.com>
2026-06-04 01:00:50 -0400

    Update 2. Select Statement Tutorial.sql

--- Course Data - Beginner/2. Select Statement Tutorial.sql
+++ Course Data - Beginner/2. Select Statement Tutorial.sql
@@ -2,6 +2,8 @@
 SELECT *
 FROM Parks_and_Recreation.employee_demographics;
 
+-- you can do age + 10 as it's own column
+-- I remembered keyword AS to rename the column
 SELECT first_name, 
 last_name, 
 birth_date,
@@ -9,5 +11,6 @@ age,
 age + 10 AS final_season_age
 FROM Parks_and_Recreation.employee_demographics;
 
+-- DISTINCT makes it such that no duplicates of a value in a given column will be returned
 SELECT DISTINCT first_name, gender
 FROM Parks_and_Recreation.employee_demographics;
